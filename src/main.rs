49732b1d575510c4f46e114d49bc1ee07d440982
Sean Francis N. Ballais <sean@seanballais.com>
2024-03-17 01:15:20 +0800

    Add base work for the IPC server.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
-use susi_core::logging;
+use std::io;
+
+use interprocess::local_socket::{LocalSocketListener, LocalSocketStream, NameTypeSupport};
+use tracing;
 use tracing_appender::non_blocking::WorkerGuard;
+use tracing_panic;
+
+use susi_core::logging;
 
 fn main() {
+    let prev_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        tracing_panic::panic_hook(panic_info);
+        prev_hook(panic_info);
+    }));
+
     let log_setup_res = logging::setup_logging();
     let mut log_guards = Vec::new();
     match log_setup_res {
@@ -16,5 +28,25 @@ fn main() {
         }
     };
 
-    tracing::info!("Testing logging mechanism.");
+    tracing::info!("Logging is ready");
+    tracing::info!("Setting up IPC server");
+
+    let socket_name = {
+        use NameTypeSupport::{Both, OnlyNamespaced, OnlyPaths};
+
+        const NAME: &str = "sfb-susi.sock";
+        match NameTypeSupport::query() {
+            OnlyPaths => format!("/tmp/{}", NAME), // For macOS support
+            OnlyNamespaced | Both => format!("@{}", NAME),
+        }
+    };
+
+    let binding_result = LocalSocketListener::bind(socket_name);
+    if binding_result.is_err() {
+        panic!("Unable to connect to socket file. Please check if the socket file is being used");
+    }
+
+    let listener = binding_result.unwrap();
+
+    tracing::info!("Starting IPC server");
 }
